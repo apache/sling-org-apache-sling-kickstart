515b26f67b99412aac09248853478d55ddda14ec
Andreas Schaefer <schaefa@iMac.local>
2020-04-03 12:08:58 -0700

    Created two scripts to launch a Composite Node Store where /apps and /libs are read only

---
+++ create_seed_fm.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+echo "-----------------------------------"
+echo "| PLEASE SHUTDOWN SLING MANUALLY  |"
+echo "| AFTER STARTUP IS COMPLETE       |"
+echo "-----------------------------------"
+
+set +x
+sleep 3
+
+rm -rf launcher
+rm -rf sling
+java -jar target/org.apache.sling.kickstart-0.0.1-SNAPSHOT.jar -af src/main/resources/feature-two-headed-seed.json -c sling/sling-composite
+
